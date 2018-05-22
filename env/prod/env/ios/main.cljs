6a485bcb458875970599c3a6e0dfb2ee9c7a6ee9
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2018-05-22 21:52:48 +0200

    Initial commit

---
+++ env/prod/env/ios/main.cljs
@@ -0,0 +1,6 @@
+ (ns env.ios.main
+  (:require [gtdorg.ios.core :as core]))
+
+ (core/init)
+
+
---
+++ test/gtdorg/core_test.clj
@@ -0,0 +1,7 @@
+(ns gtdorg.core-test
+  (:require [clojure.test :refer :all]
+            [gtdorg.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
